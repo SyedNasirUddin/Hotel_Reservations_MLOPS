d57849944cd590550eef7c25b431f4c6a4493eda
Syed Nasiruddin <syednasiruddin485@gmail.com>
2026-05-12 00:09:24 +0530

    commit

--- src/custom_exception.py
+++ src/custom_exception.py
@@ -7,7 +7,7 @@ class CustomException(Exception):
       
     @staticmethod
     def get_detailed_error_message(error_message,error_detail):
-        _,_, exc_tb=sys.exc_info()
+        _,_, exc_tb=traceback.sys.exc_info()
         file_name = exc_tb.tb_frame.f_code.co_filename
         line_number=exc_tb.tb_lineno
         return f"Error in [{file_name}], line[{line_number}]: {error_message}"
